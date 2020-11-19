c3b195d92778fef215829c56586d2e8eeccce776
Jntmkk <bbmike1086@gmail.com>
2020-11-19 17:49:42 +0800

    添加部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
-# shellcheck disable=SC2006
-export groupId=`mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.groupId -q -DforceStdout`
-export artifactId=`mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.artifactId -q -DforceStdout`
-export version=`mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout`
+export groupId=$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.groupId -q -DforceStdout)
+export artifactId=$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.artifactId -q -DforceStdout)
+export version=$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)
 export filename="${artifactId}"-"${groupId}"-"${version}"
-kill -9 `cat service.pid`
-nohup /usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /var/lib/jenkins/workspace/whh.pw/target/"${filename}".jar --spring.pofiles.active=prod  >/var/log/main.log  2>&1 &
+
+h=$(ps -aux | grep -v grep | grep "${filename}" | awk '{print $2}')
+if [ -n "$h" ]; then
+  kill -9 h
+fi
+nohup /usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /var/lib/jenkins/workspace/whh.pw/target/"${filename}".jar --spring.pofiles.active=prod >/var/log/main.log 2>&1 &
 echo $! >service.pid
