e7b52d8b60a9f8191dcca834466dcb5b1c10a613
Jntmkk <bbmike1086@gmail.com>
2020-11-19 16:21:11 +0800

    添加部署脚本

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ export artifactId=`mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate
 export version=`mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout`
 export filename="${artifactId}"-"${groupId}"-"${version}"
 kill -9 `cat service.pid`
-nohup /usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /var/lib/jenkins/workspace/whh.pw/target/${filename}.jar --spring.pofiles.active=prod  >/var/log/main.log  2>&1 &
+nohup /usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /var/lib/jenkins/workspace/whh.pw/target/"${filename}".jar --spring.pofiles.active=prod  >/var/log/main.log  2>&1 &
 echo $! >service.pid
